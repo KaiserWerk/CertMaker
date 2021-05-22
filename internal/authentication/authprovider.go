2275655559e2030d9b3dc3a43c42abb9ac68673d
Robin Kaiser <web@r-k.mx>
2021-05-22 12:32:00 +0200

    Update authprovider.go

--- internal/authentication/authprovider.go
+++ internal/authentication/authprovider.go
@@ -5,9 +5,6 @@ import "github.com/KaiserWerk/CertMaker/internal/entity"
 type AuthProvider interface {
 	AuthenticateUser(username string, password string) (*entity.User, error)
 	DeAuthenticateUser(user *entity.User) error
-	RegisterUser(user *entity.User) (int64, error)
-	UpdateUser(user *entity.User) error
-	RemoveUser(user *entity.User) error
 }
 
 
