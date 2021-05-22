276b3a38e377420982c905c1ff3efc6ad33ca610
Robin Kaiser <web@r-k.mx>
2021-05-22 21:51:21 +0200

    handler.go -> default.go

--- internal/handler/default.go
+++ internal/handler/default.go
@@ -1,11 +1,17 @@
 package handler
 
 import (
+	"fmt"
+	"github.com/KaiserWerk/CertMaker/internal/entity"
 	"github.com/KaiserWerk/CertMaker/internal/templateservice"
 	"net/http"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	val := r.Context().Value("user")
+	u := val.(entity.User)
+	fmt.Println("Username: " + u.Username)
+
 	if err := templateservice.ExecuteTemplate(w, "index.gohtml", nil); err != nil {
 		w.WriteHeader(404)
 	}
