c52d07e6092008e185c536e816ce896b327874d7
Robin Kaiser <web@r-k.mx>
2021-05-22 18:18:30 +0200

    add WithSession and RequireAdmin middleware for routes, user info can be pulled from request context

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,6 +1,12 @@
 package middleware
 
 import (
+	"context"
+	"errors"
+	"github.com/KaiserWerk/CertMaker/internal/dbservice"
+	"github.com/KaiserWerk/CertMaker/internal/global"
+	"github.com/KaiserWerk/CertMaker/internal/logging"
+	"gorm.io/gorm"
 	"net/http"
 )
 
@@ -16,13 +22,89 @@ func HeaderMiddleware(next http.Handler) http.Handler {
 
 func WithSession(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc (func(w http.ResponseWriter, r *http.Request) {
+		sessMgr := global.GetSessMgr()
+		logger := logging.GetLogger()
+		cv, err := sessMgr.GetCookieValue(r)
+		if err != nil {
+			logger.Println("no user-provided cookie found")
+			http.Redirect(w, r, "/auth/login", http.StatusSeeOther)
+			return
+		}
 
+		sess, err := sessMgr.GetSession(cv)
+		if err != nil {
+			logger.Println("could not get session: " + err.Error())
+			http.Redirect(w, r, "/auth/login", http.StatusSeeOther)
+			return
+		}
+
+		userId, ok := sess.GetVar("user_id")
+		if !ok {
+			logger.Println("session var not found")
+			http.Redirect(w, r, "/auth/login", http.StatusSeeOther)
+			return
+		}
+
+		ds := dbservice.New()
+		u, err := ds.FindUser("id = ?", userId)
+		if err != nil && errors.Is(err, gorm.ErrRecordNotFound) {
+			logger.Println("user not found: " + err.Error())
+			http.Redirect(w, r, "/auth/login", http.StatusSeeOther)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "user", u)
+		cr := r.WithContext(ctx)
+
+		next.ServeHTTP(w, cr)
+	})
+}
+
+func RequireAdmin(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc (func(w http.ResponseWriter, r *http.Request) {
+		logger := logging.GetLogger()
+		sessMgr := global.GetSessMgr()
+		cv, err := sessMgr.GetCookieValue(r)
+		if err != nil {
+			logger.Println("no user-provided cookie found")
+			http.Redirect(w, r, "/auth/login", http.StatusSeeOther)
+			return
+		}
+
+		sess, err := sessMgr.GetSession(cv)
+		if err != nil {
+			logger.Println("could not get session: " + err.Error())
+			http.Redirect(w, r, "/auth/login", http.StatusSeeOther)
+			return
+		}
+
+		userId, ok := sess.GetVar("user_id")
+		if !ok {
+			logger.Println("session var not found")
+			http.Redirect(w, r, "/auth/login", http.StatusSeeOther)
+			return
+		}
+
+		ds := dbservice.New()
+		u, err := ds.FindUser("id = ?", userId)
+		if err != nil && errors.Is(err, gorm.ErrRecordNotFound) {
+			logger.Println("user not found: " + err.Error())
+			http.Redirect(w, r, "/auth/login", http.StatusSeeOther)
+			return
+		}
+
+		if !u.Admin {
+			logger.Println("user is not an admin")
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
 
 		next.ServeHTTP(w, r)
 	})
 }
 
-func TokenMiddleware(next http.Handler) http.Handler {
+
+func WithToken(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 
