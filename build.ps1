57d32625afb13ce1f70176ca491ea79230b986cb
Robin Kaiser <web@r-k.mx>
2021-05-24 13:30:17 +0200

    rename built binaries

--- build.ps1
+++ build.ps1
@@ -1,10 +1,10 @@
 $sourcecode = "cmd/certmaker/main.go"
 $target = "build/certmaker"
 # Windows, 64-bit
-$env:GOOS = 'windows'; $env:GOARCH = 'amd64';             go build -o "$($target)-win64.exe" -ldflags "-s -w" $sourcecode
+$env:GOOS = 'windows'; $env:GOARCH = 'amd64';             go build -o "$($target)-windows-amd64.exe" -ldflags "-s -w" $sourcecode
 # Linux, 64-bit
-$env:GOOS = 'linux';   $env:GOARCH = 'amd64';             go build -o "$($target)-linux64"   -ldflags "-s -w" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'amd64';             go build -o "$($target)-linux-amd64"   -ldflags "-s -w" $sourcecode
 # Raspberry Pi
-$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=5; go build -o "$($target)-raspi32"   -ldflags "-s -w" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=5; go build -o "$($target)-raspberrypi-arm5"   -ldflags "-s -w" $sourcecode
 # macOS
-$env:GOOS = 'darwin';  $env:GOARCH = 'amd64';             go build -o "$($target)-macos64"   -ldflags "-s -w" $sourcecode
+$env:GOOS = 'darwin';  $env:GOARCH = 'amd64';             go build -o "$($target)-macos-amd64"   -ldflags "-s -w" $sourcecode
