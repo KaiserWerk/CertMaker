facfe75342cc0714d9da6f4be6555e874bbc6765
Robin Kaiser <web@r-k.mx>
2021-06-19 22:34:32 +0200

    add postgresql and mssql support

--- go.mod
+++ go.mod
@@ -4,11 +4,16 @@ go 1.16
 
 require (
 	github.com/KaiserWerk/sessionstore v1.0.1
+	github.com/denisenkom/go-mssqldb v0.10.0 // indirect
 	github.com/gorilla/mux v1.8.0
+	github.com/jackc/pgproto3/v2 v2.1.0 // indirect
 	github.com/sirupsen/logrus v1.8.1
-	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e
+	golang.org/x/text v0.3.6 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 	gorm.io/driver/mysql v1.1.0
+	gorm.io/driver/postgres v1.1.0 // indirect
 	gorm.io/driver/sqlite v1.1.4
-	gorm.io/gorm v1.21.9
+	gorm.io/driver/sqlserver v1.0.7 // indirect
+	gorm.io/gorm v1.21.11
 )
--- internal/dbservice/dbservice.go
+++ internal/dbservice/dbservice.go
@@ -5,7 +5,9 @@ import (
 	"github.com/KaiserWerk/CertMaker/internal/global"
 	"github.com/KaiserWerk/CertMaker/internal/logging"
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
+	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
 	"sync"
@@ -31,9 +33,12 @@ func New() *dbservice {
 		var driver gorm.Dialector = mysql.Open(config.Database.DSN)
 		if config.Database.Driver == "sqlite" {
 			driver = sqlite.Open(config.Database.DSN)
+		} else if config.Database.Driver == "pgsql" {
+			driver = postgres.Open(config.Database.DSN)
+		} else if config.Database.Driver == "mssql" {
+			driver = sqlserver.Open(config.Database.DSN)
 		}
-		// TODO add postgresql
-
+		
 		db, err := gorm.Open(driver, &gorm.Config{
 			PrepareStmt: true,
 			NamingStrategy: schema.NamingStrategy{
