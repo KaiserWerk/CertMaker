0432fb22d2c42caba1e0496b53b802b1276d7207
Robin Kaiser <web@r-k.mx>
2021-06-24 22:32:17 +0200

    improve the build script

--- build.ps1
+++ build.ps1
@@ -1,10 +1,12 @@
 $sourcecode = "cmd/certmaker/main.go"
 $target = "build/certmaker"
+$version = "v2.0.0"
+$date = Get-Date -Format "yyyy-MM-dd HH:mm:ss K"
 # Windows, 64-bit
-$env:GOOS = 'windows'; $env:GOARCH = 'amd64';             go build -o "$($target)-windows-amd64.exe" -ldflags "-s -w" $sourcecode
+$env:GOOS = 'windows'; $env:GOARCH = 'amd64';             go build -o "$($target)-windows-amd64-$($version).exe"  -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.VersionDate=$($date)'" $sourcecode
 # Linux, 64-bit
-$env:GOOS = 'linux';   $env:GOARCH = 'amd64';             go build -o "$($target)-linux-amd64"   -ldflags "-s -w" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'amd64';             go build -o "$($target)-linux-amd64-$($version)"        -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.VersionDate=$($date)'" $sourcecode
 # Raspberry Pi
-$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=5; go build -o "$($target)-raspberrypi-arm5"   -ldflags "-s -w" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=5; go build -o "$($target)-raspberrypi-arm5-$($version)"   -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.VersionDate=$($date)'" $sourcecode
 # macOS
-$env:GOOS = 'darwin';  $env:GOARCH = 'amd64';             go build -o "$($target)-macos-amd64"   -ldflags "-s -w" $sourcecode
+$env:GOOS = 'darwin';  $env:GOARCH = 'amd64';             go build -o "$($target)-macos-amd64-$($version)"        -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.VersionDate=$($date)'" $sourcecode
